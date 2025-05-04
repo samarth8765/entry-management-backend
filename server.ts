23e5a43c6934e40f3eae4f83120bd16da55fc64d
samarth8765 <samdhawan8765@gmail.com>
2025-05-04 14:50:14 +0530

    feat(cors): configure cors middleware

--- server.ts
+++ server.ts
@@ -21,7 +21,14 @@ export const createServer = async () => {
     return next();
   });
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ['https://entry-management-frontend.vercel.app/'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true
+    })
+  );
+
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
